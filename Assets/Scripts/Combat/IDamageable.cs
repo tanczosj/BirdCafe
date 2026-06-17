b4f1df3a82754a7fb50b231870e1f53fb43ea0b6
Joshua Tanczos <tanczosj@roverkids.org>
2026-06-17 17:06:59 -0400

    chore: removed unused assets

--- Assets/Scripts/Combat/IDamageable.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public interface IDamageable
-{
-    bool IsDead { get; }
-    void TakeDamage(float amount, Vector3 hitPoint, Vector3 hitDirection, GameObject instigator = null);
-}
